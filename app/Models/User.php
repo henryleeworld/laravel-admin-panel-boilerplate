e2f83ed9fc5f34221c24320baf64d7051d2e86bd
Henry Lee <henryleeworld@gmail.com>
2023-02-25 00:43:32 +0800

    feat:升級至 laravel 10

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Hash;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
--- database/migrations/2019_10_28_000005_create_role_user_pivot_table.php
+++ database/migrations/2019_10_28_000005_create_role_user_pivot_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoleUserPivotTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -18,4 +18,4 @@ class CreateRoleUserPivotTable extends Migration
             $table->foreign('role_id', 'role_id_fk_529106')->references('id')->on('roles')->onDelete('cascade');
         });
     }
-}
+};
